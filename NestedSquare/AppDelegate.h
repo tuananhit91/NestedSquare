81094a9dcfca7a7605cb30a009230c674a876d76
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-07-29 10:27:23 +0700

    Initial Commit

---
+++ NestedSquare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NestedSquare
+//
+//  Created by admin on 7/29/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
